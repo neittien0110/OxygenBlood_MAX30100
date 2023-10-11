c87195e05f78bde45d25d7a30b930afd87e7c7c0
tien.nguyenduc <tien.nguyenduc@hust.edu.vn>
2023-10-11 13:47:02 +0700

    first commit

---
+++ OxygenBlood_MAX30100.ino
@@ -0,0 +1,46 @@
+//------------------------------------------------------------------------------------
+// Environment:
+//  - ESP32C3 Super Mini
+//  - Arduino IDE: Board = ESP32C3 Dev Module
+//                 USB CDC on Board = Enabled
+//  - Library: MAX30100  https://www.arduino.cc/reference/en/libraries/max30100/    https://github.com/kontakt/MAX30100
+//------------------------------------------------------------------------------------
+#include <MAX30100.h>
+#include <Wire.h>
+
+#define LED_BUILTIN 8
+
+MAX30100 sensor;
+
+void setup() {
+  Wire.begin();
+  Serial.begin(115200);
+  while(!Serial);
+  sensor.begin(pw1600, i50, sr100 );
+}
+
+void loop() {
+  sensor.readSensor();
+  Serial.println(meanDiff(sensor.IR));
+  delay(10);
+}
+
+long meanDiff(int M) {
+  #define LM_SIZE 15
+  static int LM[LM_SIZE];      // LastMeasurements
+  static byte index = 0;
+  static long sum = 0;
+  static byte count = 0;
+  long avg = 0;
+
+  // keep sum updated to improve speed.
+  sum -= LM[index];
+  LM[index] = M;
+  sum += LM[index];
+  index++;
+  index = index % LM_SIZE;
+  if (count < LM_SIZE) count++;
+
+  avg = sum / count;
+  return avg - M;
+}
